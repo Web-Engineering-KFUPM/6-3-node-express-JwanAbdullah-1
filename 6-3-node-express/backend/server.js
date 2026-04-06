0d19859442cccf0e745a97a0e328b808ea773303
JwanAbdullah <s202246560@kfupm.edu.sa>
2026-04-06 13:02:51 +0300

    TODO 6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -16,9 +16,16 @@ app.use(morgan("dev"));
 const { getRandomQuote } = require("./quotes");
 
 // TODO 6.1: Create root route "/"
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote API 🚀");
+});
 
 
 // TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
 // TODO 7: Start server using app.listen
